75f15a763068b5f55fc9b0eda1cc97c92d9ed310
AlexVranesic <ales.vranesic@gmail.com>
2017-05-08 17:01:56 +0200

    Monthly payment deluje!

--- src/si/triglav/hackathon/MonthlyPayment/MonthlyPaymentController.java
+++ src/si/triglav/hackathon/MonthlyPayment/MonthlyPaymentController.java
@@ -1,4 +1,4 @@
-/*package si.triglav.hackathon.occupation;
+package si.triglav.hackathon.MonthlyPayment;
 
 import java.net.URI;
 import java.util.List;
@@ -14,54 +14,55 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import si.triglav.hackathon.RepairService.RepairService;
-import si.triglav.hackathon.product.Product;
-import si.triglav.hackathon.product.ProductDAO;
+//import si.triglav.hackathon.ContractsPolicy.ContractsPolicy;
+import si.triglav.hackathon.MonthlyPayment.MonthlyPayment;
+
 
 @Controller
-public class OccupationController {
+public class MonthlyPaymentController {
 
 	@Autowired
-	private OccupationDAO occupationDAO;
+	private MonthlyPaymentDAO monthlyPaymentDAO;
 	
-	@RequestMapping( path="/occupations/{team_key}", method=RequestMethod.GET)
-	public @ResponseBody List<Occupation> getRepairServiceList(@PathVariable(name="team_key") Integer team_key){
-		return occupationDAO.getOccupationList(team_key);
+	@RequestMapping( path="/{team_key}/monthlypayments", method=RequestMethod.GET)
+	public @ResponseBody List<MonthlyPayment> getRepairServiceList(@PathVariable(name="team_key") Integer team_key){
+		return monthlyPaymentDAO.getMonthlyPaymentList(team_key);
 	}
 		
-	//If using PathVariable, not all conversions are supported
-	@RequestMapping( path="/occupations/{team_key}/{id}", method=RequestMethod.GET)
-	public @ResponseBody Occupation getOccupationById(@PathVariable(name="id") Integer id, 
-															@PathVariable(name="team_key") Integer team_key){
-		return occupationDAO.getOccupationById(id, team_key);
+	@RequestMapping( path="/{team_key}/clients/{id_client}/monthlypayments/{id_payment}", method=RequestMethod.GET)
+	public @ResponseBody MonthlyPayment getMonthlyPaymentById(@PathVariable(name="id_payment") Integer id_payment,
+															  @PathVariable(name="id_client") Integer id_client,
+															  @PathVariable(name="team_key") Integer team_key){
+		return monthlyPaymentDAO.getMonthlyPaymentById(id_payment, id_client, team_key);
 		
 	}
 	
-	@RequestMapping( path="/occupations/{team_key}", method=RequestMethod.POST, consumes=MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<?> createOccupation(	@RequestBody Occupation occupation, 
-													@PathVariable(name="team_key") Integer team_key){
+	@RequestMapping( path="/{team_key}/clients/{id_client}/monthlypayments", method=RequestMethod.POST, consumes=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<?> createMonthlyPayment(	@RequestBody MonthlyPayment monthlyPayment, 
+													@PathVariable(name="team_key") Integer team_key,
+													@PathVariable(name="id_client") Integer id_client){
 		
 		//optionally validate repairService
-		Occupation createdOccupation = occupationDAO.createOccupation(occupation, team_key); // this will set the id on the repairService object
+		MonthlyPayment createdMonthlyPayment = monthlyPaymentDAO.createMonthlyPayment(monthlyPayment, team_key, id_client); // this will set the id on the repairService object
 		
 		URI location = ServletUriComponentsBuilder
 				.fromCurrentRequest().path("/{id}")
-				.buildAndExpand(createdOccupation.getId_occupation()).toUri();
+				.buildAndExpand(createdMonthlyPayment.getId_payment()).toUri();
 
 		//by rest conventions we need to repond with the URI for newly created resource 
 		return ResponseEntity.created(location).build();
 			
 	}
 	
-	@RequestMapping( path="/occupations/{team_key}/{id_occupation}", method=RequestMethod.PUT, consumes=MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<?> updateOccupation(	@PathVariable(name="id_occupation") Integer id_occupation,
+	@RequestMapping( path="/{team_key}/clients/{id_client}/monthlypayments/{id_payment}", method=RequestMethod.PUT, consumes=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<?> updateMonthlyPayment(	@PathVariable(name="id_payment") Integer id_payment,
+													@PathVariable(name="id_client") Integer id_client,
 													@PathVariable(name="team_key") Integer team_key,
-													@RequestBody Occupation occupation){
-		//occupation.setID_repair_service(id);
-		
-		occupation.setId_occupation(id_occupation);
-		
-		int updatedRows = occupationDAO.updateOccupation(occupation, team_key);
+													@RequestBody MonthlyPayment monthlyPayment){
+	
+		monthlyPayment.setId_payment(id_payment);
+			
+		int updatedRows = monthlyPaymentDAO.updateMonthlyPayment(monthlyPayment, team_key, id_client);
 		
 		if(updatedRows == 0 ){
 			return ResponseEntity.notFound().build();
@@ -70,19 +71,18 @@ public class OccupationController {
 		return ResponseEntity.noContent().build();
 	}
 	
-	
-	
-	
-	@RequestMapping( path="/occupations/{team_key}/{id}", method=RequestMethod.DELETE)
-	public ResponseEntity<?> deleteRepairService(	@PathVariable(name="id") Integer id,
+	//(Integer id_client, Integer id_payment, Integer team_key) {
+	@RequestMapping( path="/{team_key}/clients/{id_client}/monthlypayments/{id_payment}", method=RequestMethod.DELETE)
+	public ResponseEntity<?> deleteMonthlyPayment(	@PathVariable(name="id_payment") Integer id_payment,
+													@PathVariable(name="id_client") Integer id_client,
 													@PathVariable(name="team_key") Integer team_key){
 		
-		int updatedRows = occupationDAO.deleteOccupation(id, team_key);
+		int updatedRows = monthlyPaymentDAO.deleteMonthlyPayment(id_client, id_payment, team_key);
 		
 		if(updatedRows == 0 ){
 			return ResponseEntity.notFound().build();
 		}
 		
 		return ResponseEntity.noContent().build();
-	}	
+	}
 }
--- src/si/triglav/hackathon/MonthlyPayment/MonthlyPaymentDAO.java
+++ src/si/triglav/hackathon/MonthlyPayment/MonthlyPaymentDAO.java
@@ -1,5 +1,6 @@
-/*package si.triglav.hackathon.MonthlyPayment;
+package si.triglav.hackathon.MonthlyPayment;
 
+import java.util.Date;
 import java.util.List;
 
 import javax.sql.DataSource;
@@ -13,8 +14,11 @@ import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
+import si.triglav.hackathon.Client.ClientDAO;
+import si.triglav.hackathon.ContractsPolicy.ContractsPolicy;
+import si.triglav.hackathon.File.FileDAO;
 import si.triglav.hackathon.RepairService.RepairService;
-
+import si.triglav.hackathon.occupation.Occupation;
 import si.triglav.hackathon.team.TeamDAO;
 
 @Repository
@@ -23,85 +27,78 @@ public class MonthlyPaymentDAO {
 	@Autowired
 	private TeamDAO teamDAO;
 	
+	//@Autowired
+	//private ClientDAO clientDAO;
+	
 	private NamedParameterJdbcTemplate jdbcTemplate;
 
-	//private static final String OCCUPATION_COLUMN_LIST = "id_occupation, id_team, occupation";
-	private static final String MONTHLY_PAYMENT_COLUMN_TEAM_LIST = "id_occupation, occupation";
-	private static final String TABLE_NAME = "FREELANCE.OCCUPATION";
+	private static final String MONTHLY_PAYMENTY_COLUMN_TEAM_LIST = "id_payment, payment_value, payment_date";
+	private static final String TABLE_NAME = "FREELANCE.MONTHLY_PAYMENT";
 	
 	@Autowired
 	public void init(DataSource dataSource) {
 		this.jdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
 	}
 	
+	//this method allows occupation to be viewed by all
 	
-	 //this method allows occupation to be viewed by all
-	
-	public List<Occupation> getOccupationList(Integer team_key) {
+	public List<MonthlyPayment> getMonthlyPaymentList(Integer team_key) {
 		Integer id_team=teamDAO.getTeamIdByKey(team_key);
 		
 		MapSqlParameterSource params = new MapSqlParameterSource("id_team", id_team);
 
-		List<Occupation> occupationList = jdbcTemplate.query("select "+OCCUPATION_COLUMN_TEAM_LIST+" from "+TABLE_NAME+" WHERE id_team= :id_team ", params, new BeanPropertyRowMapper<Occupation>(Occupation.class));
-		return occupationList;
+		List<MonthlyPayment> monthlyPaymentList = jdbcTemplate.query("select "+MONTHLY_PAYMENTY_COLUMN_TEAM_LIST+" from "+TABLE_NAME+" WHERE id_team= :id_team" , params, new BeanPropertyRowMapper<MonthlyPayment>(MonthlyPayment.class));
+		return monthlyPaymentList;
 	}
 	 
-	 
-/*
-	public List<Occupation> getOccupationList() throws Exception {
+	public List<MonthlyPayment> getMonthlyPaymentList() throws Exception {
 		MapSqlParameterSource params = new MapSqlParameterSource();
-		List<Occupation> occupationList = jdbcTemplate.query("select * from schema.OCCUPATION where team_key = :team_key", params, new BeanPropertyRowMapper<Occupation>(Occupation.class));
+		List<MonthlyPayment> monthlyPaymentList = jdbcTemplate.query("select * from schema.MONTHLY_PAYMENTY where team_key = :team_key", params, new BeanPropertyRowMapper<MonthlyPayment>(MonthlyPayment.class));
 		
-		if (occupationList == null)
+		if (monthlyPaymentList == null)
 		{
-			throw new Exception("Occupation list " + occupationList + " does not exist in database!");
+			throw new Exception("Monthly payment list " + monthlyPaymentList + " does not exist in database!");
 		}
 		else
 		{
-			return occupationList;
+			return monthlyPaymentList;
 		}
 	}
-	*/
-/*	public Occupation getOccupationById(Integer id, Integer team_key) {
+	//getMonthlyPaymentById(id_payment, id_client, team_key);
+	
+	//getMonthlyPaymentById
+	public MonthlyPayment getMonthlyPaymentById(Integer ID_payment, Integer id_client, Integer team_key) {
+		
+		
 		Integer id_team=teamDAO.getTeamIdByKey(team_key);
 		
-		MapSqlParameterSource params = new MapSqlParameterSource("ID_occupation", id);
+		MapSqlParameterSource params = new MapSqlParameterSource("ID_payment", ID_payment);
 		params.addValue("id_team", id_team);
+		params.addValue("id_client", id_client);
+		
+		//params.addValue("id_team", id_team);
 		//System.out.println("===============================");
 		//System.out.println(id_team);
 		//System.out.println("===============================");
-		Occupation occupation = jdbcTemplate.queryForObject("select "+OCCUPATION_COLUMN_TEAM_LIST+" from "+TABLE_NAME+" where ID_occupation = :ID_occupation AND id_team= :id_team",params,  new BeanPropertyRowMapper<Occupation>(Occupation.class));
+		MonthlyPayment monthlyPayment = jdbcTemplate.queryForObject("select "+MONTHLY_PAYMENTY_COLUMN_TEAM_LIST+" from "+TABLE_NAME+" where ID_payment = :ID_payment AND id_client=:id_client AND id_team= :id_team",params,  new BeanPropertyRowMapper<MonthlyPayment>(MonthlyPayment.class));
 	
-		return occupation;
+		return monthlyPayment;
 	}
 	
-	
-	/*
-	 * this method allows policy to be viewed only for allowed team
-	 */
-	
-/*	public Occupation getAllowedOccupationById(Integer idOccupation) throws Exception {
-		MapSqlParameterSource params = new MapSqlParameterSource("id", idOccupation);
-		Occupation occupation = jdbcTemplate.queryForObject("select " + OCCUPATION_COLUMN_TEAM_LIST + " from schema.OCCUPATION where id = :id", params, new BeanPropertyRowMapper<Occupation>(Occupation.class));
+	public MonthlyPayment getAllowedMonthlyPaymentById(Integer id_payment) throws Exception {
+		MapSqlParameterSource params = new MapSqlParameterSource("id", id_payment);
+		MonthlyPayment monthlyPayment = jdbcTemplate.queryForObject("select " + MONTHLY_PAYMENTY_COLUMN_TEAM_LIST + " from schema.MONTHLY_PAYMENTY where id = :id", params, new BeanPropertyRowMapper<MonthlyPayment>(MonthlyPayment.class));
 		
-		//Integer currentTeamId = null;
-		/* currentTeamId = KeyChecker.getTeamKey(); naceloma se bodo oni prijavili na bazo
-		 * 												    z user + geslom. Na podlagi tega, jim baza dodali nek key, ki 
-		 *                                                  se hrani tako da nimajo oni dostop do njega.
-		 *                                                  Na tem mestu se zahteva, da program sporoci, uporabnikov ekipni key
-		 *                                                  na podlagi katerega se dovoli, alipa ne, dostop do baze
 		
-		*/
-
-/*		if (occupation == null)
+		if (monthlyPayment == null)
 		{
-			throw new Exception("Occupation with id " + idOccupation + " does not exist in database!");
+			throw new Exception("Payment with id " + id_payment + " does not exist in database!");
 		}
 		else
 		{
 //			if (occupation.getTeamId() == currentTeamId)
 //			{
-				return occupation;
+				return monthlyPayment;
 //			}
 //			else
 //			{
@@ -109,76 +106,122 @@ public class MonthlyPaymentDAO {
 //			}
 		}
 	}
-/*
-	public Occupation createOccupation(Occupation occupation) throws Exception {
-		KeyHolder generatedKeyHolder = new GeneratedKeyHolder();
-		jdbcTemplate.update(
-				"insert into schema.OCCUPATION (" + OCCUPATION_COLUMN_TEAM_LIST + ") values (:id_occupation, :id_team)",
-				new BeanPropertySqlParameterSource(occupation), generatedKeyHolder);
- 		
-		Occupation createdOccupation = getOccupationById(generatedKeyHolder.getKey().intValue());
-		//Policy createdPeson = getAllowedPolicyById(generatedKeyHolder.getKey().intValue());
-		return createdOccupation;
 
-	}
-*/
-/*	public Occupation createOccupation(Occupation occupation, Integer team_key) {
-		Integer id_team=teamDAO.getTeamIdByKey(team_key);
-
-		KeyHolder generatedKeyHolder = new GeneratedKeyHolder();
-
-		jdbcTemplate.update(
-				"insert into "+TABLE_NAME+" (occupation, id_team) values (:occupation, "+id_team+")",
-				new BeanPropertySqlParameterSource(occupation), generatedKeyHolder);
-		
-		Occupation createdOccuptaion = getOccupationById(generatedKeyHolder.getKey().intValue(), team_key);
-		return createdOccuptaion;
-
-	}
 	/*
-	public RepairService createRepairService(RepairService repairService, Integer team_key) {
+public ContractsPolicy createContractPolicy(Integer id_client, ContractsPolicy contractsPolicy, Integer team_key) {
+		
 		Integer id_team=teamDAO.getTeamIdByKey(team_key);
-
+		
+		Date actualDateFrom;
+		Date actualDateTo;
+		//for some reason it substracts a day so we add it
+		if(contractsPolicy.getDate_from()!=null)
+			actualDateFrom = new Date(contractsPolicy.getDate_from().getTime()+(24*60*60*1000));
+		else
+			actualDateFrom = null;
+		
+		if(contractsPolicy.getDate_from()!=null)
+			actualDateTo = new Date(contractsPolicy.getDate_to().getTime()+(24*60*60*1000));
+		else
+			actualDateTo = null;
+		
+		
+		MapSqlParameterSource params = new MapSqlParameterSource("date_from", actualDateFrom);
+		params.addValue("date_to", actualDateTo);
+		params.addValue("ID_client", id_client);
+		params.addValue("ID_team", id_team);
+		
 		KeyHolder generatedKeyHolder = new GeneratedKeyHolder();
 
 		jdbcTemplate.update(
-				"insert into "+TABLE_NAME+" (name, address, id_team, id_gear_type) values (:name, :address, "+id_team+", 2)",
-				new BeanPropertySqlParameterSource(repairService), generatedKeyHolder);
+				"insert into "+TABLE_NAME+" (date_from, date_to, ID_client, ID_team, ID_product) values (:date_from, :date_to, :ID_client, :ID_team, 1)",
+				params, generatedKeyHolder);
 		
-		RepairService createdRepairService = getRepairServiceById(generatedKeyHolder.getKey().intValue(), team_key);
-		return createdRepairService;
+		ContractsPolicy createdContractsPolicy = getContractsPolicyById(id_client, generatedKeyHolder.getKey().intValue(), team_key);
+		return createdContractsPolicy;
 
 	}
 	*/
-	public int updateOccupation(Occupation occupation, Integer team_key) {
+	
+	public MonthlyPayment createMonthlyPayment(MonthlyPayment monthlyPayment, Integer team_key, Integer id_client) {
 		
 		Integer id_team=teamDAO.getTeamIdByKey(team_key);
+		Date payment_date;
+		Double payment_value=null;
+		
+		if(monthlyPayment.getPayment_date()!=null)
+			payment_date = new Date(monthlyPayment.getPayment_date().getTime()+(24*60*60*1000));
+		else
+			payment_date = null;
+		
+		payment_value=monthlyPayment.getPayment_value();
+		
+		MapSqlParameterSource params = new MapSqlParameterSource("payment_date", payment_date);
+		
+		params.addValue("ID_client", id_client);
+		params.addValue("ID_team", id_team);
+		params.addValue("payment_value", payment_value);
+		
+		KeyHolder generatedKeyHolder = new GeneratedKeyHolder();
+
+		jdbcTemplate.update(
+				"insert into "+TABLE_NAME+" (payment_date, ID_client, ID_team, payment_value) values (:payment_date, :ID_client, :ID_team, :payment_value)",
+				params, generatedKeyHolder);
+		
+		MonthlyPayment createdMonthlyPayment = getMonthlyPaymentById(generatedKeyHolder.getKey().intValue() , id_client, team_key);
+		return createdMonthlyPayment;
+		
+	}
+	
+	public int updateMonthlyPayment(MonthlyPayment monthlyPayment, Integer team_key, Integer  ID_client) {
+		
+		Integer id_team=teamDAO.getTeamIdByKey(team_key);
+		Date payment_date;
+		Double payment_value=null;
+		Integer id_payment;
+		
+		id_payment=monthlyPayment.getId_payment();
+		
+		if(monthlyPayment.getPayment_date()!=null)
+			payment_date = new Date(monthlyPayment.getPayment_date().getTime()+(24*60*60*1000));
+		else
+			payment_date = null;
+		
+		payment_value=monthlyPayment.getPayment_value();
+		
+		
+		
+		MapSqlParameterSource params = new MapSqlParameterSource("payment_date", payment_date);
+		
+		params.addValue("ID_client", ID_client);
+		params.addValue("ID_team", id_team);
+		params.addValue("payment_value", payment_value);
+		params.addValue("id_payment", id_payment);
 		
 		int updatedRowsCount = jdbcTemplate.update(
 						 "UPDATE "+TABLE_NAME
-						+" SET (occupation) = (:occupation) "
-						+" WHERE ID_occupation = :id_occupation"
-						+" AND ID_team = "+id_team,
-				new BeanPropertySqlParameterSource(occupation));
+						+" SET (payment_date, payment_value) = (:payment_date, :payment_value) "
+						+" WHERE id_payment = :id_payment"
+						+" AND id_team = :ID_team"
+						+" AND ID_client = :ID_client",
+						params);
+				
 		return updatedRowsCount;
 	}
-	
-	/*
-	public int deleteOccupation(Integer id) {
+
+	public int deleteMonthlyPayment(Integer id_client, Integer id_payment, Integer team_key) {
 		
-		// tukaj lahko tudi preverjas, ce pravilna skupina brise zapis in ce ne, potem ne dovoli
-		int deletedRows = jdbcTemplate.update("delete from schema.OCCUPATION where id = :id", new MapSqlParameterSource("id", id));
-		return deletedRows;
-	}
-	*/
-	public int deleteOccupation(Integer ID_occupation, Integer team_key) {
 		Integer id_team=teamDAO.getTeamIdByKey(team_key);
 		
 		MapSqlParameterSource params = new MapSqlParameterSource("id_team", id_team);
-		params.addValue("ID_occupation", ID_occupation);
+		params.addValue("id_payment", id_payment);
+		params.addValue("ID_client", id_client);
+
 		
-		int deletedRows = jdbcTemplate.update("delete from "+TABLE_NAME+" where ID_occupation = :ID_occupation", params);
+		int deletedRows = jdbcTemplate.update(	"delete from "+TABLE_NAME
+											 +" where id_payment = :id_payment"
+											 +" AND ID_team = :id_team"
+											 +" AND ID_client = :ID_client", params);
 		return deletedRows;
-	}
-	
+	}	
 }
